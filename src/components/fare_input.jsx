8c85d568bc0441971d89ea476decfc00a80abb3f
Ian McNally <imcnally@stridenyc.com>
2015-02-13 21:48:43 -0500

    fare input - initial state and change handlers

--- src/components/fare_input.jsx
+++ src/components/fare_input.jsx
@@ -1,16 +1,36 @@
 var React = require('react');
 
 export default React.createClass({
+
+  getInitialState(){
+    return {
+      remainingBalance : null,
+      maxToSpend : 40.00
+    }
+  },
+
+  setMaxToSpend(event){
+    this.setState({
+      maxToSpend : event.target.valueAsNumber
+    });
+  },
+
+  setRemainingBalance(event){
+    this.setState({
+      remainingBalance : event.target.valueAsNumber
+    });
+  },
+
   render(){
     return (
       <form name="farepurchase">
         <label htmlFor="remaining">
           <h3>Remaining balance</h3>
-          <input name="remaining" type="number" required placeholder="0.00" />
+          <input name="remaining" value={this.state.remainingBalance} onChange={this.setRemainingBalance} type="number" required placeholder="0.00"/>
         </label>
         <label htmlFor="max">
           <h3>Max to spend</h3>
-          <input name="max" type="number" required placeholder="40.00" />
+          <input name="max" type="number" value={this.state.maxToSpend} onChange={this.setMaxToSpend} required placeholder="40.00"/>
         </label>
       </form>
     );

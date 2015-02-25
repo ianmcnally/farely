a174b814da8b010a9b26a73b8a1c0d4ae41336dd
Ian McNally <ianmcnally@gmail.com>
2015-02-25 11:51:39 -0500

    hide instructions when there are fare updates

--- src/components/instructions/instructions.jsx
+++ src/components/instructions/instructions.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Fares from '../../stores/fares';
+
+let component;
 
 export default class Instructions extends React.Component {
 
+  constructor (props) {
+    super(props);
+    component = this;
+    component.state = {
+      showInstructions : true
+    };
+  }
+
+  componentWillMount () {
+    Fares.addChangeListener(component.updateInstructions);
+  }
+
+  updateInstructions () {
+    component.setState({
+      showInstructions : !Fares.purchaseOptions.length
+    });
+  }
+
   render(){
     return (
-      <p>
+      <p hidden={!component.state.showInstructions}>
         To be or not to be.
       </p>
     );

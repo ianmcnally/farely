0b2c13de2745f0d3eac43480e8a964ab80dce7f3
Ian McNally <ianmcnally@gmail.com>
2015-03-23 21:45:59 -0400

    add footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,8 +5,8 @@ export default class Footer extends React.Component {
   render(){
     return (
       <footer>
-        <small>Updated for the 2015 fare hike.</small>
-        <small> Say hello <a href="http://twitter.com/farelyapp" target="_blank">@farelyapp</a>.</small>
+        <small>Say hello <a href="http://twitter.com/farelyapp" target="_blank">@farelyapp</a>.</small>
+        <small><a href="http://cash.me/$ianmcnally" target="_blank">Tip us.</a></small>
       </footer>
     );
   }

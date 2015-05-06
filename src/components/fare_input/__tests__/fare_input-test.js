66e0edb9c4aecc2d6f1edde14a80912e355f6766
Ian McNally <ianmcnally@gmail.com>
2015-05-05 22:32:03 -0400

    more tests for fare input

--- src/components/fare_input/__tests__/fare_input-test.js
+++ src/components/fare_input/__tests__/fare_input-test.js
@@ -3,39 +3,87 @@ jest.dontMock('../fare_input.jsx');
 import React from 'react';
 import {addons} from 'react/addons';
 import FareInput from '../fare_input.jsx';
-let TestUtils = addons.TestUtils;
+import FareActions from '../../../actions/fare_actions';
 
 describe('FareInput', () => {
 
   let fareInput;
 
   beforeEach(() => {
-    fareInput = TestUtils.renderIntoDocument(<FareInput />);
+    fareInput = addons.TestUtils.renderIntoDocument(<FareInput />);
   });
 
-  describe('remainingBalance', () => {
+  describe('initialization', () => {
 
-    let balanceInput;
-
-    beforeEach(() => {
-      balanceInput = fareInput.refs.balanceInput;
-    });
-
-    it('initializes the remainingBalance input value to null', () => {
-      expect(balanceInput.value)
+    it('sets the remainingBalance input value to null', () => {
+      expect(fareInput.refs.balanceInput.value)
         .toEqual(null);
     });
 
-  })
-
-  describe('maxToSpend', () => {
-
-    it('initializes the maxToSpend input value to 40', () => {
+    it('sets the maxToSpend input value to 40', () => {
       var maxInput = fareInput.refs.maxInput;
       expect(maxInput.props.value)
         .toEqual(40);
     });
 
-  })
+  });
+
+  describe('setting the remaining balance', () => {
+
+    let balanceInput;
+
+    beforeEach(() => {
+      balanceInput = fareInput.refs.balanceInput;
+      spyOn(FareActions, 'updateFareParameters');
+    });
+
+    it('triggers an update fare parameters action', () => {
+      let updatedValue = '3'
+
+      balanceInput.getDOMNode().value = updatedValue;
+      addons.TestUtils.Simulate.change(balanceInput.getDOMNode());
+
+      expect(FareActions.updateFareParameters).toHaveBeenCalledWith(updatedValue, '40');
+    });
+
+    it('updates the state fare parameters', () => {
+      let updatedValue = '10'
+
+      balanceInput.getDOMNode().value = updatedValue;
+      addons.TestUtils.Simulate.change(balanceInput.getDOMNode());
+
+      expect(fareInput.state.remainingBalance).toEqual(updatedValue);
+    });
+
+  });
+
+  describe('setting the spend maximum', () => {
+
+    let maxInput;
+
+    beforeEach(() => {
+      maxInput = fareInput.refs.maxInput;
+      spyOn(FareActions, 'updateFareParameters');
+    });
+
+    it('triggers an update fare parameters action', () => {
+      let updatedValue = '80'
+
+      maxInput.getDOMNode().value = updatedValue;
+      addons.TestUtils.Simulate.change(maxInput.getDOMNode());
+
+      expect(FareActions.updateFareParameters).toHaveBeenCalledWith(null, updatedValue);
+    });
+
+    it('updates the state fare parameters', () => {
+      let updatedValue = '80'
+
+      maxInput.getDOMNode().value = updatedValue;
+      addons.TestUtils.Simulate.change(maxInput.getDOMNode());
+
+      expect(fareInput.state.maxToSpend).toEqual(updatedValue);
+    });
+
+  });
 
 });

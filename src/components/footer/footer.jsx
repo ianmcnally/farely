9d052ba64fc8c74e458673c6d503d6aeac782076
Ian McNally <ianmcnally@gmail.com>
2015-12-02 20:37:58 -0500

    make footer a simple component, without render

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-export default class Footer extends React.Component {
-
-  render(){
-    return (
-      <footer>
-        <small>Say hello <a href="http://twitter.com/farelyapp" target="_blank">@farelyapp</a>.</small>
-        <small><a href="http://cash.me/$ianmcnally" target="_blank">Tip us.</a></small>
-      </footer>
-    );
-  }
-
+const Footer = () => {
+	return (
+		<footer>
+			<small>Say hello <a href="http://twitter.com/farelyapp" target="_blank">@farelyapp</a>.</small>
+			<small><a href="http://cash.me/$ianmcnally" target="_blank">Tip us.</a></small>
+		</footer>
+	)
 }
+
+export default Footer
+

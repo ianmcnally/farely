69925b8741145a21b9fbe2ea0a7dc26e166e7893
Ian McNally <ianmcnally@gmail.com>
2015-12-21 17:40:29 -0500

    fix spacing in footer file. move karma to dev dep

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
-	return (
-		<footer>
-			<small>Say hello <a href="http://twitter.com/farelyapp" target="_blank">@farelyapp</a>.</small>
-			<small><a href="http://cash.me/$ianmcnally" target="_blank">Tip us.</a></small>
-		</footer>
-	)
+  return (
+    <footer>
+      <small>Say hello <a href='http://twitter.com/farelyapp' target='_blank'>@farelyapp</a>.</small>
+      <small><a href='http://cash.me/$ianmcnally' target='_blank'>Tip us.</a></small>
+    </footer>
+  )
 }
 
 export default Footer

e005b63616d9d187828a5180dc2f61061e96347f
Ian McNally <ianmcnally@gmail.com>
2016-01-14 14:38:01 -0500

    add support consideration to footer 💸

--- src/app.jsx
+++ src/app.jsx
@@ -18,7 +18,7 @@ export default class App extends Component {
   render () {
     return (
       <Provider store={store}>
-        <section className="fare-calculator">
+        <section className='fare-calculator'>
           <FareInput />
           <PurchaseOptions />
           <Instructions />
--- src/components/purchase_options/purchase_options.jsx
+++ src/components/purchase_options/purchase_options.jsx
@@ -3,19 +3,18 @@ import { connect } from 'react-redux'
 
 export const PurchaseOptions = ({ purchaseOptions }) => {
   const children = purchaseOptions.map((option, i) => (
-    <li key={i} className="purchase-amount">
-      <span className="amount">{option.amount}</span> for {option.rides} rides
+    <li key={i} className='purchase-amount'>
+      <span className='amount'>{option.amount}</span> for {option.rides} rides
     </li>
   ))
 
-  return <ul className="purchase-amounts">{children}</ul>
+  return <ul className='purchase-amounts'>{children}</ul>
 }
 
 PurchaseOptions.propTypes = {
-  purchaseOptions: PropTypes.array.isRequired
+  purchaseOptions : PropTypes.array.isRequired
 }
 
 export const mapStateToProps = ({ purchaseOptions }) => ({ purchaseOptions })
 
 export default connect(mapStateToProps)(PurchaseOptions)
-
